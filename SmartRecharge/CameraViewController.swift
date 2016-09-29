902868ae05ea46aaa8b0c3c7405a1dd51b1e9233
Aatish Rajkarnikar <aatish.rajkarnikar@gmail.com>
2016-09-29 22:57:18 +0545

    Cropping, scaling and processing the image

--- SmartRecharge/CameraViewController.swift
+++ SmartRecharge/CameraViewController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 import AVFoundation
+import CoreImage
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet var previewView:UIView!
+    @IBOutlet var previewImageView:UIImageView!
+    @IBOutlet var boundaryRectView:UIView!{
+        didSet{
+            boundaryRectView.layer.borderColor = UIColor.red.cgColor
+            boundaryRectView.layer.borderWidth = 1
+        }
+    }
     @IBAction func capture(){
         if let videoConnection = imageOutput!.connection(withMediaType: AVMediaTypeVideo) {
             imageOutput?.captureStillImageAsynchronously(from: videoConnection, completionHandler: {(sampleBuffer, error) in
                 let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
-                let dataProvider = CGDataProvider(data: imageData as! CFData)
-                let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
-                _ = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
+                var takenImage = UIImage(data: imageData!)
+                takenImage = self.fixOrientationOfImage(image: takenImage!)
+                print(takenImage?.imageOrientation.rawValue)
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "Cropper") as! CropperViewController
+                vc.image = takenImage
+                self.present(vc, animated: true, completion: nil)
+//                let outputRect = self.previewLayer?.metadataOutputRectOfInterest(for: self.previewLayer!.bounds)
+//                let takenCGImage = takenImage?.cgImage
+//                let width = takenCGImage?.width
+//                let height = takenCGImage?.height
+//                let originX = outputRect!.origin.x
+//                let originY = outputRect!.origin.y
+//                
+//                let factorW = CGFloat(width!)/self.previewImageView.frame.width
+//                let factorH = CGFloat(height!)/self.boundaryRectView.frame.height
+//                
+//                let cropRect = CGRect(x:  originX * CGFloat(width!), y: originY * CGFloat(height!), width: outputRect!.size.width * CGFloat(width!), height: factorW * self.boundaryRectView.frame.width )
+//                let cropImage = takenCGImage?.cropping(to: cropRect)
+//                takenImage = UIImage(cgImage: cropImage!, scale: 1, orientation: (takenImage?.imageOrientation)!)
+//                
+//                self.previewImageView.image = takenImage
                 self.session?.stopRunning()
             })
         }
@@ -27,7 +53,6 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     var session:AVCaptureSession?
     var imageOutput:AVCaptureStillImageOutput?
     var previewLayer:AVCaptureVideoPreviewLayer?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -56,4 +81,63 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         previewView.layer.addSublayer(previewLayer!)
         session?.startRunning()
     }
+    
+    
+    func fixOrientationOfImage(image: UIImage) -> UIImage? {
+        if image.imageOrientation == .up {
+            return image
+        }
+        
+        // We need to calculate the proper transformation to make the image upright.
+        // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
+        var transform = CGAffineTransform.identity
+        
+        switch image.imageOrientation {
+        case .down, .downMirrored:
+            transform = transform.translatedBy(x: image.size.width, y: image.size.height)
+            transform = transform.rotated(by: CGFloat(M_PI))
+        case .left, .leftMirrored:
+            transform = transform.translatedBy(x: image.size.width, y: 0)
+            transform = transform.rotated(by: CGFloat(M_PI_2))
+        case .right, .rightMirrored:
+            transform = transform.translatedBy(x: 0, y: image.size.height)
+            transform = transform.rotated(by: -CGFloat(M_PI_2))
+        default:
+            break
+        }
+        
+        switch image.imageOrientation {
+        case .upMirrored, .downMirrored:
+            transform = transform.translatedBy(x: image.size.width, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+        case .leftMirrored, .rightMirrored:
+            transform = transform.translatedBy(x: image.size.height, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+        default:
+            break
+        }
+        
+        // Now we draw the underlying CGImage into a new context, applying the transform
+        // calculated above.
+        guard let context = CGContext(data: nil, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: image.cgImage!.bitsPerComponent, bytesPerRow: 0, space: image.cgImage!.colorSpace!, bitmapInfo: image.cgImage!.bitmapInfo.rawValue) else {
+            return nil
+        }
+        
+        context.concatenate(transform)
+        
+        switch image.imageOrientation {
+        case .left, .leftMirrored, .right, .rightMirrored:
+            context.draw(image.cgImage!, in: CGRect(x: 0, y: 0, width: image.size.height, height: image.size.width))
+        default:
+            context.draw(image.cgImage!, in: CGRect(origin: .zero, size: image.size))
+        }
+        
+        // And now we just create a new UIImage from the drawing context
+        guard let CGImage = context.makeImage() else {
+            return nil
+        }
+        
+        return UIImage(cgImage: CGImage)
+    }
 }
+
---
+++ SmartRecharge/CropperViewController.swift
@@ -0,0 +1,117 @@
+//
+//  CropperViewController.swift
+//  SmartRecharge
+//
+//  Created by Aatish Rajkarnikar on 9/27/16.
+//  Copyright © 2016 iOSHub. All rights reserved.
+//
+
+import UIKit
+
+class CropperViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet var scrollView:UIScrollView!{
+        didSet{
+            scrollView.minimumZoomScale = 1.0
+            scrollView.maximumZoomScale = 10.0
+        }
+    }
+    @IBOutlet var imageView:UIImageView!
+    @IBOutlet var cropAreaView:UIView!
+    
+    var image:UIImage?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+        print("width \(image?.size.width) height \(image?.size.height)")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    @IBAction func crop(){
+        if let image = image,let imageView = imageView{
+            let factor = image.size.width/view.frame.width
+            let scale = 1/scrollView.zoomScale
+            let imageFrame = frameForImage(image: image, inImageView: imageView)
+            let x = (scrollView.contentOffset.x + cropAreaView.frame.origin.x - imageFrame.origin.x) * scale * factor
+            let y = (scrollView.contentOffset.y + cropAreaView.frame.origin.y - imageFrame.origin.y) * scale * factor
+            
+            let visibleRect = CGRect(x: x, y: y, width: cropAreaView.frame.size.width * scale * factor, height: cropAreaView.frame.size.height * scale * factor)
+            let cgImg = image.cgImage?.cropping(to: visibleRect)
+            
+//            let imageRect = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
+//            let previewBounds = CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>)
+//            let imageCoordinate = self.frameForImage(image: image, inImageView: imageView)
+//            let imageViewFrame = imageView.frame
+//            
+//            let imageBoundsWithView = CGRect(x: imageCoordinate.origin.x + imageViewFrame.origin.x, y: imageCoordinate.origin.y + imageViewFrame.origin.y, width: imageCoordinate.width, height: imageCoordinate.height)
+//            
+//            let offset = scrollView.contentOffset
+//            
+//            let zoomedImageBounds = CGRect(x: imageBoundsWithView.origin.x - offset.x, y: imageBoundsWithView.origin.y - offset.y, width: imageBoundsWithView.width, height: imageBoundsWithView.height)
+//            
+//            let scaleFactor = imageView.image!.size.width/zoomedImageBounds.size.width
+//            
+//            let cropperFrame = CGRect(x: (view.frame.origin.x - zoomedImageBounds.origin.x) * scaleFactor, y: (view.frame.origin.y - zoomedImageBounds.origin.y) * scaleFactor, width: view.frame.size.width * scaleFactor, height: view.frame.size.height * scaleFactor)
+//            
+//            let cgImg = image.cgImage?.cropping(to: cropperFrame)
+            let img = scaleImage(image: UIImage(cgImage: cgImg!), maxDimension: 640)
+            print(img.size)
+            TesseractManager.performImageRecognition(img, completion: { (result:String) in
+                print(result)
+                let resultViewController = storyboard?.instantiateViewController(withIdentifier: "Result") as! ResultViewController
+                resultViewController.previewImage = img
+                resultViewController.previewText = result
+                self.present(resultViewController, animated: true, completion: nil)
+            })
+            
+        }
+    }
+    
+    func frameForImage(image:UIImage, inImageView imageView:UIImageView)->CGRect{
+        let imageRatio = image.size.width / image.size.height
+        let imageViewRatio = imageView.frame.size.width / imageView.frame.size.height
+        
+        if imageRatio < imageViewRatio {
+            let scaleFactor = imageView.frame.size.height / image.size.height
+            let width = image.size.width * scaleFactor
+            let topLeftX = (imageView.frame.size.width - width) * 0.5
+            return CGRect(x: topLeftX, y: 0, width: width, height: imageView.frame.size.height)
+        }else{
+            let scalFactor = imageView.frame.size.width / image.size.width
+            let height = image.size.height * scalFactor
+            let topLeftY = (imageView.frame.size.height - height) * 0.5
+            return CGRect(x: 0, y: topLeftY, width: imageView.frame.size.width, height: height)
+        }
+    }
+
+    func scaleImage(image: UIImage, maxDimension: CGFloat) -> UIImage {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        var scaleFactor:CGFloat
+        
+        if image.size.width > image.size.height {
+            scaleFactor = image.size.height / image.size.width
+            scaledSize.width = maxDimension
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            scaleFactor = image.size.width / image.size.height
+            scaledSize.height = maxDimension
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: scaledSize.width, height: scaledSize.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage!
+    }
+}
